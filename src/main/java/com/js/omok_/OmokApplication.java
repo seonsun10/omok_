1d1f451b1295d79548c2c703f1f10dec622f68ef
psw <dbxlf3018@gmail.com>
2024-11-26 09:02:37 +0900

    기본 정보 커밋

---
+++ src/main/java/com/js/omok_/OmokApplication.java
@@ -0,0 +1,13 @@
+package com.js.omok_;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OmokApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OmokApplication.class, args);
+    }
+
+}
